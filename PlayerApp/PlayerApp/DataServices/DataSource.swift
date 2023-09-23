df01bd3117e39585becf77327ad34f4a552dab0a
Sergey Ruppel <sergeyruppel@gmail.com>
2023-09-23 17:54:13 +0700

    Part 1

---
+++ PlayerApp/PlayerApp/DataServices/DataSource.swift
@@ -0,0 +1,17 @@
+//
+//  DataSource.swift
+//  PlayerApp
+//
+//  Created by Sergey Ruppel on 14.09.2023.
+//
+
+import Foundation
+
+class DataSource {
+    static var trackList = [Track(fileName: "Rakhim - Синий Lamborghini", fileExtension: "mp3"),
+                            Track(fileName: "Ваня Дмитриенко - Венера-Юпитер", fileExtension: "mp3"),
+                            Track(fileName: "Rauf & Faik, NILETTO - Если тебе будет грустно", fileExtension: "mp3"),
+                            Track(fileName: "Rasa - Под фонарём", fileExtension: "mp3"),
+                            Track(fileName: "Нурминский - Валим", fileExtension: "mp3"),
+                            Track(fileName: "Нурминский - Валим", fileExtension: "mov")]
+}
---
+++ PlayerApp/PlayerApp/Models/Track.swift
@@ -0,0 +1,13 @@
+//
+//  Track.swift
+//  PlayerApp
+//
+//  Created by Sergey Ruppel on 14.09.2023.
+//
+
+import Foundation
+
+struct Track {
+    let fileName: String
+    let fileExtension: String
+}
